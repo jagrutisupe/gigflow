0327d686b43447953448a015e0554ba96ac38348
jagrutisupe <jagrutisupe@gmail.com>
2026-05-17 14:47:49 +0530

    Fix index.ts CORS setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -11,9 +11,17 @@ dotenv.config();
 connectDB();
 
 const app = express();
+
+app.options('*', cors());
 app.use(cors({
-  origin: ['https://gigflow-phi-peach.vercel.app', 'http://localhost:5173'],
-  credentials: true
+  origin: [
+    'https://gigflow-phi-peach.vercel.app',
+    'http://localhost:5173',
+    'http://localhost:3000'
+  ],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(express.json());
 app.use(morgan('dev'));
